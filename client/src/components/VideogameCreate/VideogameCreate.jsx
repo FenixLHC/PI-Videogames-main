cfa612bee6aa1360fdefb2a2759fd4a6ab9a58b2
LHC <lautarohconti@gmail.com>
2022-09-24 06:10:19 -0300

    Validatios done

--- client/src/components/VideogameCreate/VideogameCreate.jsx
+++ client/src/components/VideogameCreate/VideogameCreate.jsx
@@ -6,6 +6,8 @@ import { createVideogame, getGenres, getPlataforms } from "../../redux/actions";
 import { isEmptyObj } from "../../utils";
 
 export default function VideogameCreate() {
+
+//LOCAL STATES AND HOOKS///////////////////////////////////////////////////
   const [videogame, setVideogame] = useState({
     name: "",
     description: "",
@@ -15,34 +17,86 @@ export default function VideogameCreate() {
     platforms: [],
   });
   const dispatch = useDispatch();
-  const history=useHistory();
+  const history = useHistory();
   const genres = useSelector((state) => state.genres);
   const platformsFromApi = useSelector((state) => state.platforms);
-  let videogameCreated=useSelector(state=>state.videogameCreated);
-  videogameCreated={}
+  let videogameCreated = useSelector(state => state.videogameCreated);
+  videogameCreated = {}
 
+//ERRORS STATES////////////////////////////////////////////////////////////  
+  const [errorName, setErrorName] = useState('')
+  const [errorDate, setErrorDate] = useState('')
+  const [errorRating, setErrorRating] = useState('')
+  const [errorPlatforms, setErrorPlatforms] = useState('')
+
+//USEEFFECT////////////////////////////////////////////////////////////////
   useEffect(() => {
     dispatch(getPlataforms());
     dispatch(getGenres());
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (isEmptyObj(videogameCreated)) return 'it may cause an error'
-    else{
+    else {
       alert('Videogame created succesfully');
       console.log(videogameCreated);
-      videogameCreated={}
+      videogameCreated = {}
       history.push('/Home')
     }
-  },[videogameCreated])
+  }, [videogameCreated])
+//VALIDATIONS//////////////////////////////////////////////////////////////
+  function validateName(name) {
+    //string 30 chars//4 characters minimum//not special chars ñÑáÁéÉíÍóÓuÚ
+    const regExName = /^[a-zA-Z0-9\s]{4,30}$/
+    if (regExName.test(name)) {
+      setErrorName('')
+      return true
+    }
+    else {
+      setErrorName('Invalid: name can not have special characters');
+      return false
+    }
+  }
 
+  function validateDate(released) {
+    //posterior a 1950 //no superior a fecha actual
+    let date = new Date(released)
+    let minDate = new Date('01/01/1950')
+    if (date <= Date.now() && date >= minDate) {
+      setErrorDate('')
+      return true
+    }
+    else {
+      setErrorDate('Invalid: Date has to be between 1950-now');
+      return false
+    }
+  }
+
+  function validateRating(rating) {
+    //float //2 decimales //entre 0 y 5
+    const regExRating = /^\s*-?[1-5]\d*(\.\d{1,2})?\s*$/
+    if (regExRating.test(rating)) {
+      setErrorRating('')
+      return true
+    }
+    else {
+      setErrorRating('Invalid: Rating has to be between 1-5, with 2 decimal places');
+      return false
+    }
+  }
+
+  function validatePlatforms(platforms) {
+    //no puede estar vacio
+    if (platforms.length >= 1) { setErrorPlatforms(''); return true } else {setErrorPlatforms('Invalid: There has to be at least one Platform')}
+  }
+//HANDLERS////////////////////////////////////////////////////////////////
   const handleOnchange = (e) => {
     setVideogame({
       ...videogame,
       [e.target.name]: e.target.value,
     });
   };
-  
+
   const handleCheckbox = (e) => {
     if (e.target.checked) {
       setVideogame({
@@ -52,7 +106,7 @@ export default function VideogameCreate() {
     }
     console.log(videogame);
   };
-  
+
   const handleSelect = (e) => {
     setVideogame({
       ...videogame,
@@ -60,52 +114,70 @@ export default function VideogameCreate() {
     });
     console.log(videogame)
   };
-  
+
   const handleOnsubmit = (e) => {
     e.preventDefault();
-    dispatch(createVideogame(videogame));
-    setVideogame({
-      name: "",
-      description: "",
-      releaseDate: "",
-      rating: "",
-      genres: [],
-      platforms: [],
-    });
+    // validatePlatforms(videogame.platforms)
+    if (errorName || errorRating || errorDate || videogame.platforms.length===0) {
+      validatePlatforms(videogame.platforms)
+      alert('There may be invalid data')
+    } else {
+      dispatch(createVideogame(videogame));
+      setVideogame({
+        name: "",
+        description: "",
+        releaseDate: "",
+        rating: "",
+        genres: [],
+        platforms: [],
+      });
+    }
   };
 
+  //RENDER///////////////////////////////////////////////////////////////
   return (
     <div>
       <Link to={"/Home"}>Home</Link>
       <h1>Create your own Videogame</h1>
       <form onSubmit={handleOnsubmit}>
+        {/* NAME */}
         <div>
           <label>Name: </label>
           <input
             name="name"
             value={videogame.name}
             type="text"
-            onChange={(e) => handleOnchange(e)}
+            onChange={(e) => { handleOnchange(e); validateName(e.target.value) }}
+            required
           />
+          {!errorName ? null : <span>{errorName}</span>}
         </div>
+        {/* DATE OF RELEASED */}
         <div>
           <label>Date of release: </label>
           <input
             name="releaseDate"
             value={videogame.releaseDate}
             type="date"
-            onChange={(e) => handleOnchange(e)}
+            onChange={(e) => { handleOnchange(e); validateDate(e.target.value) }}
+            required
           />
+          {!errorDate ? null : <span>{errorDate}</span>}
         </div>
+        {/* RATING */}
         <div>
           <label>Rating: </label>
           <input
             name="rating"
             value={videogame.rating}
             type="float"
-            onChange={(e) => handleOnchange(e)}
+            //min={0}
+            //max={5}
+            onChange={(e) => { handleOnchange(e); validateRating(e.target.value) }}
           />
+          {!errorRating ? null : <span>{errorRating}</span>}
         </div>
+        {/* PLATFORMS */}
         <div>
           <label>Platforms</label>
           {platformsFromApi?.map((p) => {
@@ -122,9 +194,11 @@ export default function VideogameCreate() {
               </label>
             );
           })}
+          {!errorPlatforms?null: <span>{errorPlatforms}</span>}
         </div>
+        {/* GENRES */}
         <div>
-          <select  onChange={(e) => handleSelect(e)}>
+          <select onChange={(e) => handleSelect(e)}>
             <option>Genres</option>
             {genres?.map((g) => {
               return (
@@ -132,15 +206,16 @@ export default function VideogameCreate() {
                   key={g.id}
                   value={g.name}
                   name={g.name}
-                  // onChange={handleSelect}
+                // onChange={handleSelect}
                 >
                   {g.name}
                 </option>
               );
             })}
           </select>
-          <ul><li>{videogame.genres.map(g=>`-${g}-`)}</li></ul>
+          <ul><li>{videogame.genres.map(g => `-${g}-`)}</li></ul>
         </div>
+        {/* DESCRIPTION */}
         <div>
           <label>Description: </label>
           <input
@@ -148,6 +223,7 @@ export default function VideogameCreate() {
             value={videogame.description}
             type="text"
             onChange={(e) => handleOnchange(e)}
+            required
           />
         </div>
         <button type="submit">Create</button>
